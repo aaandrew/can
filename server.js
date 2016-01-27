fd0df0202f63d7fb3dbedee836da26a9a8bcc301
Andrew Du <aadu@ucsd.edu>
2016-01-26 21:58:13 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var handlebars = require('express-handlebars');
+
+var app = express();
+
+// Configuration
+app.engine('handlebars', handlebars({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.set('port', 3000);
+
+// Load routes
+require('./app/routes/routes')(app);
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
