08402ebcd91afb8fc1fd0e8253d441153212f43e
Cynthia Hong <cynthiahong911@gmail.com>
2016-01-27 20:23:45 -0800

    Update1

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+function intializedPage{
+$("#submitBtn").click(updateProject);
+}
+
+function updateProject(e) {
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
+}
+
