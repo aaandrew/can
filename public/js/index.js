61e5c6391f9827912869ce290613f99679857615
Andrew Du <aadu@ucsd.edu>
2016-02-20 20:38:45 -0800

    navbar

--- public/js/index.js
+++ public/js/index.js
@@ -12,3 +12,19 @@ function updateProject(e) {
    $(projectID + " .project-description").text(newText);
 }
 
+$(document).ready(function(){
+
+  /******** Navbar code ********/
+
+  // Hamburger animation
+  $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+    $(this).toggleClass('open');
+  });
+
+  // Slide out animation
+  $('.nav-toggle').on('click', function() {
+    $('.main-navigation').toggleClass('open');
+  });
+
+  
+});
