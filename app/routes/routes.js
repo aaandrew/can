df5716df72ba8437527041873a49238c28510bbf
Andrew Du <aadu@ucsd.edu>
2016-03-03 00:34:24 -0800

    rip forgot something

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -223,7 +223,7 @@ module.exports = function (app, passport) {
 			var apt = {};
 			// Set date
 			var dateString = moment(appointments[i].date + "/" + curYear, "MM-DD-YYYY");
-			apt.day = moment(dateString).format("dddd, MMMM Do YYYY");
+			apt.day = moment(dateString).format("MMMM Do, YYYY");
 			apt.time = appointments[i].time;
 			apt.name = appointments[i].mentor.name;
 			apt.college = generator.getCollegeName(appointments[i].mentor.college);
